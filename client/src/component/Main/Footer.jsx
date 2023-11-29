9b49e0ccafcab004401177d3d3ac6c5157db17a8
Yaseen Shaikh <yaseenpadha7296@gmail.com>
2023-11-29 19:01:26 +0530

    make it better

--- client/src/component/Main/Footer.jsx
+++ client/src/component/Main/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (<>
 <footer>
         <div class="footer center">
-            Created By <a class="footname" href="https://www.instagram.com/yaseen07_/" target="_blank">Yaseen</a> Copyright &copy; 2023 amazonconstruction.com All rights reserved
+            Created By <a class="footname" href="https://www.instagram.com/yaseen07_/" target="_blank">Yaseen</a> Copyright &copy; 2024 amazonconstruction.com All rights reserved
         </div>
 </footer>
   </>);
